8f27b2b0859221bb32c4e3cb8d155b7386d7b992
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-05 01:22:31 +0300

    Tests passed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    
+    def __init__(self, discount = 0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+
+    def add_item(self,prod_name, prod_cost, quantity = 1):
+            self.cart = prod_cost * quantity
+            self.total += self.cart
+            for x in range(quantity):
+                self.items.append(prod_name)
+      
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = int((self.discount/100) * self.total)
+            self.total -= discount_amount
+            print(f"After the discount, the total comes to ${self.total:}.")
+        else:
+            print("There is no discount to apply.")
+        
+    def void_last_transaction(self):
+            self.total -= self.cart
+            return self.total 
+    
+        
+
+
+    
+    
+
